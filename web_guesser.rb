1eccec7a33384cdb0d0a87721518313dc6176b41
em55 <blue.emailid@gmail.com>
2016-05-13 15:08:26 +0530

    I4: output colorized

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,35 +3,41 @@ require 'sinatra/reloader'
 
 
 x = rand(100)
+
 		
+get '/' do
+	
+	guess = params['guess'].to_i
+	m = check_guess(x,guess)
+	erb :index, :locals => {:x => x, :m => m}
+
+end
+
 def check_guess(n,g)
 	
-	diff = n - g
+	diff = g - n
 	if diff == 0
-			return "You got it right! <br> The secret number is #{n}" 
-	elsif diff > 5
-			return "Way too low!"
-	elsif diff <= 5
-			return "Too low!"
+			
+			return "<style> body { background: green;} </style> 
+					You got it right! <br> The secret number is #{n}" 
 	elsif diff < -5
-			return "Way too high!"
-	elsif diff >= -5
-			return "Too high!"
+			
+			return "<style> body { background: red;} </style>
+					Way too low!"
+	elsif diff > 5
+			return "<style> body { background: red;} </style>
+					Way too high!"
+	elsif (diff > 0 && diff <=5)
+			return "<style> body { background: pink; color: black;} </style>
+					Too high!"
+	elsif (diff < 0 && diff >= -5)
+			
+			return "<style> body { background: pink; color: black;} </style>
+					Too low!"
+	
 									
 	end
 end
 
-
-get '/' do
-	
-	guess = params['guess'].to_i
-
-	
-	m = check_guess(x,guess)
-
-	erb :index, :locals => {:x => x, :m => m}
-
-	
-end
 
 
